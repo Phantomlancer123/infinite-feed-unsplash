897805b3a9828e11333f6b8bea752fda0892c358
MightyDevKnight <strongdragon036@gmail.com>
2022-11-09 15:50:06 +0900

    set infinitescroll page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import styled from 'styled-components';
 import { getPhotos } from './services/unsplashService';
 import './App.css';
+
+const LandingPage = styled.div`
+  margin-top: 10vh;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  min-height: 100vh;
+  background: #212121;
+  font-family: sans-serif;
+`;
+
+const WrapperCard = styled.div`
+  width: 1200px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 45px;
+  margin: 0 auto;
+`;
 
 function App() {
   const [data, setData] = useState([]);
@@ -17,11 +37,19 @@ function App() {
   console.log("data: ", data);
 
   return (
-    <div className="App">
-      {data.map(pic => (
-        <image src={pic.urls.small} />
-      ))}
-    </div>
+    <LandingPage>
+      <InfiniteScroll
+        dataLength={data.length}
+        next={loadPhotos}
+        hasMore={true}
+      >
+        <WrapperCard>
+          {data.map(pic => (
+            <image width='100%' height='20vw' src={pic.urls.small} />
+          ))}
+        </WrapperCard>
+      </InfiniteScroll>
+    </LandingPage>
   );
 }
 
